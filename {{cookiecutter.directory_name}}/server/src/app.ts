83063add9915ba5da4e35dc454ffcece21d2d20c
Nicholas Cannon <nicholascannon1@gmail.com>
2021-11-07 13:08:59 +0800

    async error wrapper

--- {{cookiecutter.directory_name}}/server/src/app.ts
+++ {{cookiecutter.directory_name}}/server/src/app.ts
@@ -4,6 +4,7 @@ import { Environment } from './env';
 import { getLogger } from './log';
 import { makeRequestLogger } from './middleware/request-logger';
 import { getRedactedEnvironment } from './utils/environment-parser';
+import { asyncErrorWrapper } from './utils/express-handler-wrappers';
 
 export const startApp = (env: Environment): Server => {
   const logger = getLogger();
@@ -14,9 +15,12 @@ export const startApp = (env: Environment): Server => {
   app.use(express.json());
   app.use(makeRequestLogger(logger));
 
-  app.get('/', (_req, res) => {
-    return res.json({ message: 'Working ' });
-  });
+  app.get(
+    '/',
+    asyncErrorWrapper((_req, res) => {
+      return res.json({ message: 'Working ' });
+    })
+  );
 
   logger.info('Application setup complete');
   return app.listen(env.port, () => logger.info('Server started...', { port: env.port }));
