a5a0324ef8335cc88a01e6b076617a5f00aa3137
Nicholas Cannon <nicholascannon1@gmail.com>
2021-11-07 15:38:56 +0800

    fix logger

--- {{cookiecutter.project_name}}/server/src/log.ts
+++ {{cookiecutter.project_name}}/server/src/log.ts
@@ -3,6 +3,6 @@ import Logger, { createLogger, stdSerializers } from 'bunyan';
 let logger: Logger | undefined;
 
 export const getLogger = (): Logger => {
-  if (!logger) logger = createLogger({ name: '{{{cookiecutter.project_name}}', serializers: stdSerializers });
+  if (!logger) logger = createLogger({ name: '{{cookiecutter.project_name}}', serializers: stdSerializers });
   return logger;
 };
