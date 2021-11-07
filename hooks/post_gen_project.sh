a03597dc981b6c11c7443bf297c060f8b174bf9c
Nicholas Cannon <nicholascannon1@gmail.com>
2021-11-07 17:42:33 +0800

    add react init

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+git init
+
+if [[ "{{cookiecutter.create_react_app(y/n)}}" == "y" ]]; then
+  cd client
+  rm .gitkeep
+  npx create-react-app .
+  rm -rf .git/ # remove cra's git folder
+  rm .gitignore
+  rm README.md
+  cd ..
+fi
+
 cd server && npm i
