aec546393d0b0f767bd2d8469b6f06de52d70002
Nicholas Cannon <nicholascannon1@gmail.com>
2022-03-07 19:38:13 +0800

    remove uuid package

--- {{cookiecutter.project_name}}/server/src/middleware/request-logger.ts
+++ {{cookiecutter.project_name}}/server/src/middleware/request-logger.ts
@@ -1,11 +1,11 @@
 import Logger from 'bunyan';
 import { RequestHandler } from 'express';
-import { v4 as uuid } from 'uuid';
+import { randomUUID } from 'crypto';
 
 export const createRequestLogger =
   (logger: Logger): RequestHandler =>
   (req, res, next) => {
-    const requestId = uuid();
+    const requestId = randomUUID();
     const startTime = Date.now();
     req.requestId = requestId;
 
