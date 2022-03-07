073537413284641f69f9d65f4597be1336a49deb
Nicholas Cannon <nicholascannon1@gmail.com>
2022-03-07 16:43:38 +0800

    app refactor

--- {{cookiecutter.project_name}}/server/src/app.ts
+++ {{cookiecutter.project_name}}/server/src/app.ts
@@ -1,6 +1,7 @@
-import express, { Application } from 'express';
+import express from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
+import { Server } from 'http';
 import { errorHandler } from './error';
 import { getLogger } from './log';
 import { makeRequestLogger } from './middleware/request-logger';
@@ -12,24 +13,47 @@ interface ApplicationConfig {
   corsOrigins?: string[];
 }
 
-export const makeApp = (config: ApplicationConfig): Application => {
+export const createApp = (config: ApplicationConfig) => {
   const logger = getLogger();
-  logger.info('Creating application...');
-
   const app = express();
+  let server: Server | undefined;
 
   app.use(helmet());
   if (config.corsOrigins?.length) app.use(cors({ origin: config.corsOrigins }));
   app.use(express.json());
 
+  // non request logging routes
   app.get('/healthcheck', asyncErrorWrapper(makeHealthCheckRoute(config.version)));
 
   app.use(makeRequestLogger(logger));
 
-  /* Routes here */
+  // routes here
 
   app.use(errorHandler);
 
-  logger.info('Application creation complete');
-  return app;
+  return {
+    _app: app,
+
+    listen: (port: number) => {
+      server = app.listen(port, () => logger.info('Server started...', { port }));
+      return app;
+    },
+
+    close: () => {
+      if (!server) {
+        logger.warn('Attempting to close server that is not started');
+        return;
+      }
+
+      logger.info('Stopping server...');
+      server.close((err) => {
+        if (err) {
+          logger.error('Could not stop server', { err });
+          process.exit(1);
+        }
+
+        logger.info('Server stopped');
+      });
+    },
+  };
 };
--- {{cookiecutter.project_name}}/server/src/index.ts
+++ {{cookiecutter.project_name}}/server/src/index.ts
@@ -1,28 +1,15 @@
-import { makeApp } from './app';
+import { createApp } from './app';
 import { parseEnvironment } from './env';
 import { getLogger } from './log';
 import { redactSecrets } from './utils/environment-utils';
 import { registerLifecycleEvents, getLifecycle } from './utils/lifecycle';
 
-registerLifecycleEvents(getLifecycle());
-
-const logger = getLogger();
+const lifecycle = getLifecycle();
+registerLifecycleEvents(lifecycle);
 
 const env = parseEnvironment();
-logger.info('Parsed environment', redactSecrets({ ...env }));
+getLogger().info('Parsed environment', redactSecrets({ ...env }));
 
-const appConfig = { version: env.version, corsOrigins: env.corsOrigins };
-const app = makeApp(appConfig);
-
-const server = app.listen(env.port, () => logger.info('Server started...', { port: env.port }));
-
-getLifecycle().on('close', async () => {
-  server.close((err) => {
-    if (err) {
-      logger.error('Could not stop server', { err });
-      process.exit(1);
-    } else {
-      logger.info('Server stopped');
-    }
-  });
-});
+const app = createApp({ version: env.version, corsOrigins: env.corsOrigins });
+app.listen(env.port);
+lifecycle.on('close', async () => app.close());
