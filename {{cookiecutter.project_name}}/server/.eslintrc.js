225a9d73f098d415e14a4ebdf2a450e918c0cd32
Nicholas Cannon <nicholascannon1@gmail.com>
2022-06-11 14:30:04 +0800

    remove google stlying

--- {{cookiecutter.project_name}}/server/.eslintrc.js
+++ {{cookiecutter.project_name}}/server/.eslintrc.js
@@ -4,7 +4,6 @@ module.exports = {
         es2021: true,
     },
     extends: [
-        'google', 
         'plugin:prettier/recommended',
         'plugin:@typescript-eslint/eslint-recommended',
         'plugin:@typescript-eslint/recommended',
