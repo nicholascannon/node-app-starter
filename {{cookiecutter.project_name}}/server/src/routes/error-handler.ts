8d77ea70e34d89570ce23ae224c463c52c7b7185
Nicholas Cannon <nicholascannon1@gmail.com>
2022-07-02 18:31:09 +0800

    bunch of small updates, renames and fixes

--- {{cookiecutter.project_name}}/server/src/routes/error-handler.ts
+++ {{cookiecutter.project_name}}/server/src/routes/error-handler.ts
@@ -4,6 +4,10 @@ import { Logger } from '../utils/logger';
 export const createGlobalErrorHandler =
     (logger: Logger): ErrorRequestHandler =>
     (error, req, res, next) => {
+        if (res.headersSent) {
+            return next(error);
+        }
+
         logger.error('Internal server error', { error });
         return res.status(500).json({ message: 'Internal server error' });
     };
