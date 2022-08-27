e73e12ca28869db47effc460138dd14928d1ea2a
nicholascannon <nicholascannon1@gmail.com>
2022-08-27 09:53:12 +0800

    remove CONFIG dependency from app.ts

--- {{cookiecutter.project_name}}/server/src/app.ts
+++ {{cookiecutter.project_name}}/server/src/app.ts
@@ -1,17 +1,16 @@
 import express from 'express';
 import helmet from 'helmet';
 import { errorHandler } from './middleware/error-handler';
-import { CONFIG } from './config';
 import { requestLogger } from './utils/logger';
 
-export const createApp = () => {
+export const createApp = (version: string) => {
     const app = express();
 
     app.use(helmet());
     app.use(express.json());
     app.use(requestLogger);
 
-    app.get('/healthcheck', (_req, res) => res.status(200).json({ message: 'Ok', version: CONFIG.version }));
+    app.get('/healthcheck', (_req, res) => res.status(200).json({ message: 'Ok', version }));
 
     app.use(errorHandler);
 
--- {{cookiecutter.project_name}}/server/src/index.ts
+++ {{cookiecutter.project_name}}/server/src/index.ts
@@ -16,7 +16,7 @@ logger.info('Config', {
     port: CONFIG.port,
 });
 
-const app = createApp();
+const app = createApp(CONFIG.version);
 const server = app.listen(CONFIG.port, () => {
     logger.info('Service started', { port: CONFIG.port });
     lifecycle.on('close', () => server.close(() => logger.info('Service stopped')));
